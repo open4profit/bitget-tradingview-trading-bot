3fdf7758dee928ddd82c0b8ad6ab8a6cdf3aaa23
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-16 19:03:37 +0000

    fgfg

--- Index.js
+++ Index.js
@@ -18,12 +18,7 @@ const futuresClient = new FuturesClient({
     apiPass: API_PASS,
 }, );
 
-const tp = parseFloat(parseFloat(price) - (parseFloat(price) * TAKE_PROFIT_PERCENT / 100)).toFixed(2)
 
-const sl = parseFloat(parseFloat(price) + (parseFloat(price) * STOP_LOSS_PERCENT / 100)).toFixed(2)
-
-    const presetTakeProfitPrice = tp;
-    const presetStopLossPrice = sl;
 
 //beta
 const replaceMultipler = (number, decimals, multipler) => {
@@ -97,7 +92,12 @@ app.post('/api/v1/placeorder', (req, res) => {
     const triggerPrice = req.body.triggerPrice;
     const triggerType = 'market_price';
 
-    
+    const tp = parseFloat(parseFloat(price) - (parseFloat(price) * TAKE_PROFIT_PERCENT / 100)).toFixed(2);
+
+    const sl = parseFloat(parseFloat(price) + (parseFloat(price) * STOP_LOSS_PERCENT / 100)).toFixed(2);
+
+    const presetTakeProfitPrice = tp;
+    const presetStopLossPrice = sl;
    
     // const symbol = 'SBTCSUSDT_SUMCBL';
     // const marginCoin = 'SUSDT';
