c91cae8705919eb26cb65ea009e62dd2d679a8c6
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-17 07:57:21 +0000

    asdf

--- Index.js
+++ Index.js
@@ -89,10 +89,10 @@ app.post('/api/v1/placeorder', (req, res) => {
     const leverage = req.body.leverage;
     const planType = req.body.planType;
     const rangeRate = req.body.rangeRate;
-    const triggerPrice = req.body.triggerPrice;
+    const triggerPrice = req.body.triggerPrice.toFixed(2);
     const triggerType = 'market_price';
-    const tp = parseFloat(parseFloat(price) - (parseFloat(price) * TAKE_PROFIT_PERCENT / 100)).toFixed(1);
-    const sl = parseFloat(parseFloat(price) + (parseFloat(price) * STOP_LOSS_PERCENT / 100)).toFixed(1);    
+    const tp = parseFloat(parseFloat(price) - (parseFloat(price) * TAKE_PROFIT_PERCENT / 100)).toFixed(2);
+    const sl = parseFloat(parseFloat(price) + (parseFloat(price) * STOP_LOSS_PERCENT / 100)).toFixed(2);    
     const presetTakeProfitPrice = tp;    
     const presetStopLossPrice = sl;
     console.log(`presetStopLossPrice: ${tp}`);
