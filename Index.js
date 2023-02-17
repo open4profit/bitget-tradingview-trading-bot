23000e26924f2e1b33d9cd85c91500563c85f763
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-17 05:27:20 +0000

    xcv

--- Index.js
+++ Index.js
@@ -97,7 +97,7 @@ app.post('/api/v1/placeorder', (req, res) => {
     const sl = parseFloat(parseFloat(price) + (parseFloat(price) * STOP_LOSS_PERCENT / 100)).toFixed(2);
     
     const presetTakeProfitPrice = tp;
-    console.log(`presetTakeProfitPrice: ${presetTakeProfitPrice}`);
+    console.log(presetTakeProfitPrice);
     const presetStopLossPrice = 2500;
    
     // const symbol = 'SBTCSUSDT_SUMCBL';
