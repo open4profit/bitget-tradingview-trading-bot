e6e0bfe07e65719eb8b0e3fcc81a3b54e68bf31a
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-19 00:45:22 +0000

    jhkj

--- Index.js
+++ Index.js
@@ -110,11 +110,11 @@ app.post('/api/v1/placeorder', async (req, res) => {
     const STOP_LOSS_PERCENT  = req.body.STOP_LOSS_PERCENT;
     const orderType = req.body.orderType;
     if(sl>cmp){
-        const side = 'open_short';
+         side = 'open_short';
     } else {
-        const side = 'open_long';
+         side = 'open_long';
     }
-    //const side = req.body.side;
+    const side = side;
     const price = parseFloat(req.body.price).toFixed(pricePlace);
     const amountBuyPAIR1  = parseFloat(req.body.AMOUNT * req.body.leverage) / parseFloat(req.body.price);
     const size = parseFloat(amountBuyPAIR1).toFixed(sizePlace);
