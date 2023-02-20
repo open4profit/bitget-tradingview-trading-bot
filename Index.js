389907d306f28e55b9b3157dbd2a3cf032addfe3
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2023-02-20 10:00:14 +0000

    dd

--- Index.js
+++ Index.js
@@ -131,7 +131,7 @@ app.post('/api/v1/placeorder', async (req, res) => {
         stoploss1 = parseFloat(parseFloat(price) - (parseFloat(price) * (STOP_LOSS_PERCENT / leverage) / 100)).toFixed(pricePlace);
    }
     const tp = tp1;
-    const stoploss = sl;    
+    const stoploss = stoploss1;    
     const presetTakeProfitPrice = replaceMultipler(tp, pricePlace, multiplier);    
     const presetStopLossPrice = replaceMultipler(stoploss, pricePlace, multiplier);
     console.log(`presetStopLossPrice: ${stoploss}`);
